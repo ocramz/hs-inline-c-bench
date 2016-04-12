b23e439ec9fe699bc732d2a69c8682590ef486de
ocramz <zocca.marco@gmail.com>
2016-04-12 14:44:02 +0200

    'Unexpected Identifier' Word32 error

--- inline-c-bench/src/InlineC.hs
+++ inline-c-bench/src/InlineC.hs
@@ -4,8 +4,13 @@ module InlineC where
 import Internal
 import qualified Language.C.Inline as C
 
+import Data.Word (Word32)
+import Foreign.C.Types (CSize(..))
+
 C.context ctx
 
 C.include "<test.h>"
 
 
+queueInit' q buf sz =
+  [C.exp|void{queue_init($(qut q),$(Word32* buf),$(CSize sz))}|]
--- inline-c-bench/src/PutGet.hs
+++ inline-c-bench/src/PutGet.hs
@@ -1,3 +1,6 @@
 module PutGet where
 
 import InlineC
+
+queueInit' q buf sz =
+  [C.exp|void{queue_init($(qut q),$(Word32* buf),$(CSize sz))}|]
